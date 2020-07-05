89b8f31ca0b374cb04b284c64d4a24b8b5a6f47f
DiegocV <azmaeldcv@gmail.com>
2020-07-05 18:00:39 -0400

    Cabecera ip completo, avance con cabecera tcp, falta datos tcp y posible UDP

--- Captura_paquetes.py
+++ Captura_paquetes.py
@@ -1,26 +1,93 @@
 import socket, sys
 from struct import*
 
-#Creamos un socket
+#Creamos un socket para recivir paquetes de tipo TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
 contador = 1
 
-# recibimos un packet
+
+def whatflag(flags):
+  if flags == 16384:
+    return "No Divisible"
+  else: 
+    return "Divisible"
+
+# recibimos 10 paquetes
 while contador <= 10:
   packet = s.recvfrom(65535)
   packet = packet[0]
 
   info_ip = packet[0:20]
-  ip_f = unpack('!BBHHHBBH4s4s' , info_ip)
-  version = ip_f[0]
-  ttl = ip_f[5]
-  protocolo = ip_f[6]
-  ip_origen =  socket.inet_ntoa(ip_f[8])
-  ip_destino = socket.inet_ntoa(ip_f[9])
-  print 'version: '+str(version)
-  print 'TTL: ' + str(ttl)
-  print 'Protocolo: ' + str(protocolo)
-  print 'IP origen: '+ str(ip_origen)
-  print 'IP destino '+ str(ip_destino)
+  cabecera_ip = unpack('!BBHHHBBH4s4s' , info_ip)
+
+  version_IHL = cabecera_ip[0]
+  version = version_IHL >> 4
+  ihl = version_IHL & 0xF
+  longitud_iph = ihl*4  
+
+  tos = cabecera_ip[1]
+  longitud_total = cabecera_ip[2]
+  ip_id = cabecera_ip[3]
+  flags = cabecera_ip[4]
+  ttl = cabecera_ip[5]
+  protocolo = cabecera_ip[6]
+  checksum = cabecera_ip[7]
+  ip_origen =  socket.inet_ntoa(cabecera_ip[8])
+  ip_destino = socket.inet_ntoa(cabecera_ip[9])
+  print('##########################################################################################')
+  print("Datos recividos: ",packet)
+  
+  print('version de IP: ' + str(version))         #1
+  print('IHL: ' +str(ihl)+ ' es decir: '+ str(ihl*32//8) + ' bytes')                       #1
+  print('TOS: ' + str(tos))                       #2
+  print('IP Total Length: '+ str(longitud_total)+' es decir: '+ str(longitud_total*32//8)+ ' bytes') #3
+  print('ID: '+ str(ip_id))                            #4
+  print('Flags :' + str(flags)) +" "+ whatflag(flags)                      #5
+  print('TTL: ' + str(ttl))                       #6
+  print('Protocolo: ' + str(protocolo))           #7
+  print('Cheksum: '+ str(checksum))               #8
+  print('IP origen: '+ str(ip_origen))            #9
+  print('IP destino '+ str(ip_destino))           #10
+  
+  print("**************INFORMACION DE TCP********************")
+  # ahora para la parte de TCP
+  info_tcp = packet[longitud_iph:longitud_iph+20]
+  cabecera_tcp = unpack('!HHLLBBHHH',info_tcp)
+  
+
+
+
+
+
+
+
+  '''
+  puerto_orig = cabecera_tcp[0]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[1]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[2]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[3]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[4]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[5]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[6]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[7]
+  print("dat "+ str(puerto_orig))
+  puerto_orig = cabecera_tcp[8]
+  print("dat "+ str(puerto_orig))
+  '''
+
+
+
+
   contador = contador + 1 
+
+
+
+
